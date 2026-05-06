e6c64f8c519950b1bd3b90f1fab062aa3b792734
NAGA_RU1 <gvodintsova@gmail.com>
2026-05-06 10:15:33 +0300

    Скорректировано третье задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,9 +9,9 @@ Student.prototype.setSubject = function(subjectName) {
 	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function(...marksToAdd) {
-	if (this.hasOwnProperty('marks') && this.marks !== undefined) {
-		this.marks.push(...marksToAdd);
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks !== undefined) {
+		this.marks.push(...marks);
 	}
 }
 
